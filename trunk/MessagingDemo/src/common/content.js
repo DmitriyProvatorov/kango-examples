734f4717895bdf73ef9ad3ba43119b28dc420cc3
kadotex@gmail.com <kadotex@gmail.com@73c5e22d-f40e-aed8-bf4d-a59e9b1a2cba>
2012-02-10 14:34:11 +0000

    Kango 0.9.3

---
+++ trunk/MessagingDemo/src/common/content.js
@@ -0,0 +1,22 @@
+﻿// ==UserScript==
+// @name Test
+// @include http://*
+// @include https://*
+// ==/UserScript==
+
+if(window == window.top) {
+	function postPageInfoMessage() {
+		var pageInfo = {
+			url: document.location.href
+		};
+		// post messsage to background script
+		kango.console.log('Sending info...');
+		kango.dispatchMessage('PageInfo', pageInfo);
+	}
+	
+	postPageInfoMessage();
+	
+	kango.addMessageListener('GetPageInfo', function(event) {
+		postPageInfoMessage();
+	});
+}
---
+++ trunk/PopupDemo/src/common/main.js
@@ -0,0 +1,11 @@
+﻿function MyExtension() {
+	//kango.ui.browserButton.setPopup({url:'test.html', width: 600, height:596})
+	//kango.ui.browserButton.setPopup(null)
+}
+
+MyExtension.prototype = {
+};
+
+kango.ui.addEventListener(kango.ui.event.Ready, function() {
+	return new MyExtension();
+});
